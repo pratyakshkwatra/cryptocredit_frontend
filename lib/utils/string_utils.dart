f2eae3533a9b012d7682cb4d66431c85babcd564
pratyakshkwatra <pratyakshkwatra@gmail.com>
2025-08-11 09:51:32 +0530

    util: add variable percentage @ string utils

--- lib/utils/string_utils.dart
+++ lib/utils/string_utils.dart
@@ -1,4 +1,4 @@
-String shortenAddress(String address) {
+String shortenAddress(String address, {double? percentage}) {
   if (address.length <= 10) return address;
-  return '${address.substring(0, 5)}...${address.substring(address.length - (address.length * 0.2).round())}';
+  return '${address.substring(0, 5)}...${address.substring(address.length - (address.length * (percentage ?? 0.2)).round())}';
 }
